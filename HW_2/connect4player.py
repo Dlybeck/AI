bea2efd423f6093ea242a920631de55960dee282
dlybeck <dlybeck383@gmail.com>
2024-02-24 00:42:31 -0800

    Works Properly on 1 ply but breaks on more

--- HW_2/connect4player.py
+++ HW_2/connect4player.py
@@ -10,14 +10,24 @@ import time
 
 class ComputerPlayer:
     def __init__(self, id, difficulty_level):
-        self.id = id
-        self.depth = difficulty_level
         """
         Constructor, takes a difficulty level (likely the # of plies to look
         ahead), and a player ID that's either 1 or 2 that tells the player what
         its number is.
         """
+        self.id = 3-id
+
+        self.depth = difficulty_level
+
+        self.width = None
+        self.height = None
+
         pass
+
+    class State:
+        def __init__(self, score, col_Index):
+            self.score = score
+            self.move = col_Index
 
     def pick_move(self, rack):
         """
@@ -28,47 +38,84 @@ class ComputerPlayer:
         drop a disc. The player current just pauses for half a second (for 
         effect), and then chooses a random valid move.
         """
-        evaluate(rack);
 
-    def evaluate(self, rack):
+        rack = [list(col) for col in rack]
+
+        self.width = len(rack)
+        self.height = len(rack[0])
+
+        move = self.negamax(rack, self.id, self.depth)
+        print("Playing move with score ", move.score)
+        return move.move
+
+
+    def negamax(self, rack, id, depth):
+        if depth == 0 or self.Is_Game_Over(rack):
+            return self.State(self.evaluate(rack, id) * (1 if id == self.id else -1), None)
+
+        best_State = self.State(-float('inf'), None)  # Initialize best_State with a very low score
+        scores = []
+        for col in range(self.width):
+            for disc in range(self.height):
+                # Can play here
+                if rack[col][disc] == 0:
+                    rack[col][disc] = id  # Make the move
+
+                    # Recurse
+                    new_State = self.negamax(rack, 3 - id, depth - 1)
+
+                    scores.append(new_State.score)
+                    # If this move is better than a previous move, update best_State
+                    if new_State.score > best_State.score:
+                        best_State.score = new_State.score
+                        best_State.move = col
+
+                    rack[col][disc] = 0  # Reset the move to make the next move
+                    break  # To only look at one move per column
+        
+        print("DEPTH ", depth, " picked ", best_State.score, " out of ", scores)
+        return self.State(best_State.score * (1 if id == self.id else -1), best_State.move)
+
+
+
+    def Is_Game_Over(self, rack):
+        full = True
+
+        for col in range(self.width):
+            if(rack[col][self.height-1] == 0): full = False
+        return full
+
+    def evaluate(self, rack, id):
         looked_at = 0
         total_score = 0
         #Check all horizontal quartets
-        for col in range(len(rack)-3):
-            for slot in range(len(rack[col])):
+        for col in range(self.width-3):
+            for slot in range(self.height):
                 looked_at += 1
                 quartet = (rack[col][slot], rack[col+1][slot], rack[col+2][slot], rack[col+3][slot])
-                total_score += self.Score_Quartet(quartet)
-        print("Looked at ", looked_at, " quartets")
-
+                total_score += self.Score_Quartet(quartet, id)
         #check for all the vertical quartets
-        for col in range(len(rack)):
-            for slot in range(len(rack[col])-3):
+        for col in range(self.width):
+            for slot in range(self.height-3):
                 looked_at += 1
                 quartet = (rack[col][slot], rack[col][slot+1], rack[col][slot+2], rack[col][slot+3])
-                total_score += self.Score_Quartet(quartet)
-        print("Looked at ", looked_at, " quartets")
-
+                total_score += self.Score_Quartet(quartet, id)
         #check low to high diagonals
-        for col in range(len(rack)-3):
-            for slot in range(len(rack[col])-3):
+        for col in range(self.width-3):
+            for slot in range(self.height-3):
                 looked_at += 1
                 quartet = (rack[col][slot], rack[col+1][slot+1], rack[col+2][slot+2], rack[col+3][slot+3])
-                total_score += self.Score_Quartet(quartet)
-        print("Looked at ", looked_at, " quartets")
-
+                total_score += self.Score_Quartet(quartet, id)
         #check high to low diagonals
-        for col in range(len(rack)-3):
-            for slot in range(len(rack[col])-3):
+        for col in range(self.width-3):
+            for slot in range(self.height-3):
                 slot +=3
                 looked_at += 1
                 quartet = (rack[col][slot], rack[col+1][slot-1], rack[col+2][slot-2], rack[col+3][slot-3])
-                total_score += self.Score_Quartet(quartet)
-        print("Looked at ", looked_at, " quartets")
+                total_score += self.Score_Quartet(quartet, id)
+        return total_score
 
-        print("Total Score is ", total_score)
-
-    def Score_Quartet(self, quartet):
+    def Score_Quartet(self, quartet, id):
         whos_quartet = 0 #variable to keep track of what player has the first disc in this quarted
         count = 0
         for i in range(len(quartet)):
@@ -85,33 +132,19 @@ class ComputerPlayer:
             else:
                 count = 0
                 break
-        
         if(count != 0 and whos_quartet != 0):
-            if(count == 0): return 0
-            if(whos_quartet == self.id):
+            if(whos_quartet == id):
                 #Positive
                 if(count == 1): return 1
                 elif(count == 2): return 10
                 elif(count == 3): return 100
-                else: return 100000
+                else: return 1000000
             else:
                 #negative
                 if(count == 1): return -1
                 elif(count == 2): return -10
                 elif(count == 3): return -100
-                else: return -100000
+                else: return -1000000
+                
         else:
             return 0
-
-player = ComputerPlayer(1, 4)
-
-'''
-THE BOARD IS UPSIDE DOWN
-'''
-player.evaluate([[0, 0, 0, 0, 0, 0],
-                 [0, 0, 0, 0, 0, 0],
-                 [0, 0, 0, 0, 0, 0],
-                 [0, 0, 0, 0, 0, 0],
-                 [0, 0, 0, 0, 0, 0],
-                 [0, 0, 0, 0, 0, 0],
-                 [0, 0, 0, 0, 0, 0]])
