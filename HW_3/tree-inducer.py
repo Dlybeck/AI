f07ee89b7562b9fa188564dee9f0c7169fd36547
dlybeck383 <dlybeck@pugetsound.edu>
2024-03-21 00:32:16 -0700

    Incorrect Information Gain

--- HW_3/tree-inducer.py
+++ HW_3/tree-inducer.py
@@ -1,6 +1,17 @@
 import sys
 import math
 
+class node:
+    def __init__(self):
+        next_Yea = None #if there is another node for yea, link here
+        yea = None #label for yea if this is a leaf
+        
+        next_Nay = None
+        nay = None
+        
+        next_Other = None
+        other = None
+    
 class rep:
     def __init__(self, id, label, votes):
         self.id = id
@@ -11,13 +22,13 @@ class rep:
         return self.id + " "+ self.label + " " + self.votes
   
 def parse_arguments():
-    print("This is the name of the script: ", sys.argv[0])
-    print("Number of arguments: ", len(sys.argv))
-    print("The arguments are: " , str(sys.argv))
+    #print("This is the name of the script: ", sys.argv[0])
+    #print("Number of arguments: ", len(sys.argv))
+    #print("The arguments are: " , str(sys.argv))
     
     if (len(sys.argv) > 2):
         print("Too many arguments given");
-        sys.exit
+        sys.exit()
     
     fileName = sys.argv[1]
     
@@ -35,7 +46,7 @@ def parse_arguments():
         #Split up string into parts
         info = repStrings[i].split()
         reps[i] = rep(info[0], info[1], info[2])
-        print(reps[i])
+        #print(reps[i])
     
     return reps
     
@@ -47,6 +58,11 @@ def split_data(data):
         else: training_set.append(data[i]) #create training set
         
     return training_set, tuning_set
+
+def log(num):
+    if(num == 0):
+        return 0
+    else: return math.log2(num)
 
 def create_decision_tree(training_set):
     numA = 0
@@ -60,11 +76,13 @@ def create_decision_tree(training_set):
     probA = numA/len(training_set)
     probB = numB/len(training_set)
     
-    set_entropy = (-probA)*math.log2(probA) + (-probB)*math.log2(probB)
+    set_entropy = -((probA)*log(probA) + (probB)*log(probB))
 
-    current_info = 0
-    max_info = 0
+    info_gain = 0
+    max_info_gain = 0
+    max_info_index = None
     current_entropy = 0
+    
     #for each vote listed
     for i in range(len(training_set[0].votes)):
         y = 0
@@ -75,18 +93,23 @@ def create_decision_tree(training_set):
             if(rep.votes[i] == '+'): y += 1
             elif(rep.votes[i] == '-'): n += 1
             else: other += 1
-        #print("For issue ", i, " there were ", y, " +'s, ", n, " -'s and ", other, " abstains")
-        
+        print("For issue ", i, " there were ", y, " +'s, ", n, " -'s and ", other, " abstains")
         
         probY = y/len(training_set)
         probN = n/len(training_set)
         probOther = other/len(training_set)
         
-        current_entropy = (-probY)*math.log2(probY) + (-probN)*math.log2(probN) + (-probOther)*math.log2(probOther)
-        current_info = set_entropy - current_entropy
+        current_entropy = -((probY)*log(probY) + (probN)*log(probN) + (probOther)*log(probOther))
+        info_gain = set_entropy - current_entropy
+        
+        print("     information gain is ", info_gain)
         
         #adjust max entropy
-        if(current_entropy > max_entropy): max_entropy = current_entropy
+        if(info_gain > max_info_gain): 
+            max_info_gain = info_gain
+            max_info_index = i
+        
+    print("Max information gain is ", max_info_gain, " at issue ", max_info_index)
         
         
 
