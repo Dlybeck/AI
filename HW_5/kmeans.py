3c8df7a900ffbaf106e5ede6a55e67a8f80af6ca
dlybeck <dlybeck383@gmail.com>
2024-04-18 13:46:58 -0700

    Working!!!

--- HW_5/kmeans.py
+++ HW_5/kmeans.py
@@ -1,5 +1,5 @@
 import sys
-
+import copy
 '''
 This class is used to hold all of a representatives data such as their id, what their label is
 and their voting history
@@ -9,6 +9,7 @@ class rep:
         self.id = id
         self.label = label
         self.coords = coords
+        self.group = -1
         
     def __str__ (self):
         coord_Str = ""
@@ -75,7 +76,7 @@ def find_Start(reps, n):
             rep1 = reps[i]
             rep2 = reps[j]
             if rep1 != rep2:  # Skip duplicates
-                distance = find_Distance(rep1, rep2)
+                distance = find_Distance(rep1.coords, rep2.coords)
                 distances.append((distance, (rep1, rep2)))
 
     #Sort distances by distance (x[0])
@@ -92,28 +93,71 @@ def find_Start(reps, n):
             if len(farthest_reps) == n:
                 break
 
+    max_coords = []
     max_reps = []
     i = 0
-    while (i < len(farthest_reps)) and len(max_reps) < n:
+    while (i < len(farthest_reps)) and len(max_coords) < n:
         distance, (rep1, rep2) = farthest_reps[i]
-        if rep1 not in max_reps:
-            max_reps.append(rep1)
-        if rep2 not in max_reps and len(max_reps) < n:
-            max_reps.append(rep2)
+        if rep1.coords not in max_coords:
+            max_coords.append(rep1.coords)
+            max_reps.append(rep1.id)
+        if rep2.coords not in max_coords and len(max_coords) < n:
+            max_coords.append(rep2.coords)
+            max_reps.append(rep2.id)
         i += 1
 
-    return list(max_reps)
+    print()
+    print("Initial centroids based on: ", ', '.join(max_reps))
 
+    return max_coords
 
-
-def find_Distance(rep1, rep2):
-    coords1 = rep1.coords
-    coords2 = rep2.coords
-
+def find_Distance(coords1, coords2):
     squared_Dists = []
     for i in range(len(coords1)):
         squared_Dists.append((coords1[i] - coords2[i]) ** 2)
     return sum(squared_Dists)
+
+def E_Step(data, centroids):
+    #for every point
+    for rep in data:
+        #find the best classification
+        best_Dist = None
+        group = 0
+        for cent in centroids:
+            dist = find_Distance(rep.coords, cent)
+            if(best_Dist == None) or (dist < best_Dist):
+                best_Dist = dist
+                rep.group = group
+
+            group += 1
+
+#Move centroids to center of group
+def M_Step(data, centroids):
+    # num of reps in each group (initialized to 0)
+    group_Sizes = [0] * len(centroids)
+    group_Sums = []
+    for i in range(len(centroids)):
+        group_Sums.append([0] * len(centroids[0]))
+
+    for rep in data:
+        group_Sizes[rep.group] += 1
+        for i in range(len(rep.coords)):
+            group_Sums[rep.group][i] += rep.coords[i]
+
+    # Update the centroids with the new centroid positions
+    for i in range(len(group_Sizes)):
+        if group_Sizes[i] > 0:
+            centroids[i] = [num / group_Sizes[i] for num in group_Sums[i]]
+        else:
+            centroids[i] = [0] * len(centroids[i])
+
+
+def sameData(data1, data2):
+    for i in range(len(data1)):
+        if(data1[i].group != data2[i].group): return False
+    return True
+
+
 
 '''
 Main method
@@ -121,8 +165,35 @@ Main method
 if __name__ == "__main__":
     data, n = parse_arguments()
 
-    maxReps = find_Start(data, n)
+    centroids = find_Start(data, n)
+    old_Centroids = []
+    rounds = 0
+    while(old_Centroids != centroids):
+        rounds+=1
+        old_Centroids = copy.deepcopy(centroids)
+        E_Step(data, centroids)
+        M_Step(data, centroids)
+    
+    print("Converged after ", rounds, " rounds of k-means.")
+    for i in range(len(centroids)):
+        size = 0
+        Ds = 0
+        Rs = 0
 
-    for rep in maxReps:
-        print(rep)
+        for rep in data:
+            if(rep.group == i):
+                if(rep.group == i): size+=1
+                if(rep.label == 'D'): Ds+=1
+                if(rep.label == 'R'): Rs+=1
+
+        Ds = (Ds/size)*100
+        Rs = (Rs/size)*100
+        print("   Group" + str(i+1)+ ":  size "+ str(size)+ " ("+ str(round(Ds, 3))+ "% D, "+ str(round(Rs, 3))+ "% R)")
+
+
+    
+    
+
+
+
     
